b163306d6f3732da3920cacb0f9422342be2c613
TatiJordan <jordan_perez.tatiana@tiernoparla.es>
2022-09-27 20:09:24 +0200

    terminado ejercicio print pares version 1- for por pares

--- src/main/java/com/tiernoparla/ejercicioselementales/EjerciciosElementales.java
+++ src/main/java/com/tiernoparla/ejercicioselementales/EjerciciosElementales.java
@@ -3,6 +3,13 @@ package com.tiernoparla.ejercicioselementales;
 
 public class EjerciciosElementales {
     public static void main(String[] args) {
-        System.out.println("hola");
+        
+        // printear nums pares del 1 al 10
+      for (int i = 2; i < 11; i=i+2){
+          System.out.println(i);
+      }// for    
+      
+        
+        
     }//main
 }//ejercicios elementales
