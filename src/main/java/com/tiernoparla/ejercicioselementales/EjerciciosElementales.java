3c9e605c4ab7a3bbde953940c62a1763801aaebf
TatiJordan <jordan_perez.tatiana@tiernoparla.es>
2022-09-27 18:51:35 +0200

    añado main class

---
+++ src/main/java/com/tiernoparla/ejercicioselementales/EjerciciosElementales.java
@@ -0,0 +1,8 @@
+
+package com.tiernoparla.ejercicioselementales;
+
+public class EjerciciosElementales {
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }//main
+}//ejercicios elementales
