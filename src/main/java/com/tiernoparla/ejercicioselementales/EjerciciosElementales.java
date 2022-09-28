28795cc989d17dd338fa801c77a606eeae124b8b
alendez5 <alejandro.menendezcanache@tiernoparla.es>
2022-09-28 17:54:14 +0200

    añadimos otra forma de hacer el ejercicio

--- src/main/java/com/tiernoparla/ejercicioselementales/EjerciciosElementales.java
+++ src/main/java/com/tiernoparla/ejercicioselementales/EjerciciosElementales.java
@@ -4,11 +4,21 @@ package com.tiernoparla.ejercicioselementales;
 public class EjerciciosElementales {
     public static void main(String[] args) {
         
-        // printear nums pares del 1 al 10
-      for (int i = 2; i < 11; i=i+2){
-          System.out.println(i);
-      }// for    
-      
+        // printear nums pares del 1 al 10 2 forma
+        for (int i = 1; i <= 10; i++){
+             if(i%2 == 0) {
+                 System.out.println(i);
+             }//if
+        }//for 2 forma
+        
+        
+        
+        /*
+        // printear nums pares del 1 al 10 con for 
+        for (int i = 2; i < 11; i=i+2){
+            System.out.println(i);
+        }// for    
+        */
         
         
     }//main
