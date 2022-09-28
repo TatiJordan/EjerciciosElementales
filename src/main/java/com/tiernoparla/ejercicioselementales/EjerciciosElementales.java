d439c18b2cc48d1d783cf82c2e473718b968ee34
alendez5 <alejandro.menendezcanache@tiernoparla.es>
2022-09-28 18:15:37 +0200

    printear del 1 al 10 con while e if

--- src/main/java/com/tiernoparla/ejercicioselementales/EjerciciosElementales.java
+++ src/main/java/com/tiernoparla/ejercicioselementales/EjerciciosElementales.java
@@ -2,15 +2,35 @@
 package com.tiernoparla.ejercicioselementales;
 
 public class EjerciciosElementales {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+       
+        //printear nums pares del 1 al 10 3 forma 
+        int n = 1;
+        while( n < 11 ) {
+            if (n%2 == 0){
+                System.out.println(n);
+            }//if
+            n++;
+        }//while
         
-        // printear nums pares del 1 al 10 2 forma
+        /*
+        //listar los numeros entre el 0 y antes del 11.
+        int n = 0;
+        while (n < 11) {
+            System.out.println(n);
+            n++;
+        }//while
+        */
+        
+        
+        /*
+        //printear nums pares del 1 al 10 2 forma
         for (int i = 1; i <= 10; i++){
              if(i%2 == 0) {
                  System.out.println(i);
              }//if
         }//for 2 forma
-        
+        */
         
         
         /*
